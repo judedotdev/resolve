2076f4ffef415ebac9e8a7c8608b632585003b73
Jude <silentcodear@gmail.com>
2024-12-23 11:32:51 +0100

    refactor

--- resolve-backend/server.js
+++ resolve-backend/server.js
@@ -57,4 +57,11 @@ export default app; // Safe and standard practice
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+
+    app.get('/', (req, res) => {
+        console.log(`Server is running on http://${req.hostname}:${PORT}`);
+        res.status(200).send('Server Started Successfully!');
+    });
+});
